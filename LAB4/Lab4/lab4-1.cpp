774e823cf7c6064e3197e02097bc384761760445
Martin Hjalmarsson <martin.hja@gmail.com>
2016-12-07 12:29:10 +0100

    Started on lab4

---
+++ LAB4/Lab4/lab4-1.cpp
@@ -0,0 +1,276 @@
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <cstdlib>  
+#include <iostream>
+
+#include "readfile.hpp"
+
+#define GLM_FORCE_RADIANS
+#include <glm/vec3.hpp> // glm::vec3
+#include <glm/vec4.hpp> // glm::vec4
+#include <glm/mat4x4.hpp> // glm::mat4
+#include <glm/gtc/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
+#include <glm/gtc/type_ptr.hpp> //glm::value_ptr
+
+#ifndef M_PI
+#define M_PI 3.14159265359
+#endif
+
+glm::vec4 mouse_state = glm::vec4(0);
+glm::vec3 position, dir, right, up = glm::vec3( 0, 0, 0 ); 
+
+float delta_time= 0.0f;
+float yaw = M_PI;
+float pitch = 0;
+float focal_dist = 512.0f;
+float move_speed = 0.08; 
+float mouse_speed = 0.001f;
+
+int w_width = 512;
+int w_height = 512;
+bool first_frame = true;
+
+void checkShaderCompileError(GLint shaderID)
+{
+  GLint isCompiled = 0;
+  glGetShaderiv(shaderID, GL_COMPILE_STATUS, &isCompiled);
+ 
+  if(isCompiled == GL_FALSE)
+  {
+    GLint maxLength = 0;
+    glGetShaderiv(shaderID, GL_INFO_LOG_LENGTH, &maxLength);
+
+    // The maxLength includes the NULL character
+    std::string errorLog;
+    errorLog.resize(maxLength);
+    glGetShaderInfoLog(shaderID, maxLength, &maxLength, &errorLog[0]);
+
+    std::cout << "shader compilation failed:" << std::endl;
+    std::cout << errorLog << std::endl;
+    return;
+  }
+  else
+    std::cout << "shader compilation success." << std::endl;
+    
+  return;
+}
+
+
+static void error_callback(int error, const char* description)
+{
+    std::cerr << description;
+}
+
+static void scroll_callback(GLFWwindow* window, double scroll_v, double scroll_h)
+{
+  mouse_state.x += scroll_v;
+  mouse_state.y += scroll_h;
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+  // Move forward
+  if ( (key == GLFW_KEY_UP   || key == GLFW_KEY_W) && 
+       (action == GLFW_PRESS || action == GLFW_REPEAT))
+  {
+    position += dir * move_speed;
+  }
+  // Move backward
+  if ( (key == GLFW_KEY_DOWN || key == GLFW_KEY_S) && 
+       (action == GLFW_PRESS || action == GLFW_REPEAT))
+  {
+    position -= dir * move_speed;
+  }
+  // Strafe right
+  if ( (key == GLFW_KEY_RIGHT|| key == GLFW_KEY_D) && 
+       (action == GLFW_PRESS || action == GLFW_REPEAT))
+  {
+    position += right * move_speed;
+  }
+  // Strafe left
+  if ( (key == GLFW_KEY_LEFT || key == GLFW_KEY_A) && 
+       (action == GLFW_PRESS || action == GLFW_REPEAT))
+  {
+    position -= right * move_speed;
+  }
+    if ((key == GLFW_KEY_ESCAPE || key == GLFW_KEY_Q) && action == GLFW_PRESS )
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+void update_camera_basis()
+{
+  dir = glm::vec3(cos(pitch) * sin(yaw), 
+                  sin(pitch),
+                  cos(pitch) * cos(yaw));
+
+  right = glm::vec3(  sin(yaw - M_PI/2.0f), 
+                                0,
+                                cos(yaw - M_PI/2.0f));
+  up = glm::cross( right, dir );
+}
+
+static void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
+{
+
+  //only happens once
+  static double previous_time = glfwGetTime();
+  double current_time = glfwGetTime();
+  delta_time = static_cast<float>(current_time - previous_time);
+
+  // Reset mouse position
+  glfwSetCursorPos(window, w_width/2.0f, w_height/2.0f);
+
+  if(first_frame){  
+    update_camera_basis();
+    first_frame = false; 
+    yaw = M_PI;
+    pitch = 0;
+
+    return;
+  }
+
+  // Compute new orientation
+  yaw   += mouse_speed * float( w_width/2.0f - xpos );
+  pitch += mouse_speed * float( w_height/2.0f - ypos );
+  update_camera_basis();
+    
+  previous_time = current_time;
+}
+
+static void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+  if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+    mouse_state.z = 1;
+
+  if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
+    mouse_state.w = 1;
+
+  if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE)
+    mouse_state.z = 0;
+
+  if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_RELEASE)
+    mouse_state.w = 0;
+}
+
+static void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+
+int main(int argc, char const *argv[])
+{
+  // start GL context and O/S window using the GLFW helper library
+  
+  glfwSetErrorCallback(error_callback);
+  if( !glfwInit() )
+    exit(EXIT_FAILURE);
+  
+  GLFWwindow* window = glfwCreateWindow (w_width, w_height, "Hello Raycasting", NULL, NULL);
+  glfwSetKeyCallback(window, key_callback);
+  glfwSetCursorPosCallback(window, cursor_position_callback);
+  glfwSetMouseButtonCallback(window, mouse_button_callback);
+  glfwSetScrollCallback ( window, scroll_callback); 
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+  glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+  glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+
+  if (!window) {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+  }
+  glfwMakeContextCurrent (window);
+                                  
+  // start GLEW extension handler
+  glewExperimental = GL_TRUE;
+  glewInit ();
+
+  //this will be the image plane: two triangles, spanning the entire screen.
+  float points[] = {
+    -1, -1, 0,
+    -1, +1, 0,
+    +1, -1, 0,
+    +1, +1, 0
+  };
+
+  position = glm::vec3( 0.5, 0.5, 2.5);
+
+  GLuint vbo = 0;
+  glGenBuffers (1, &vbo);
+  glBindBuffer (GL_ARRAY_BUFFER, vbo);
+  glBufferData (GL_ARRAY_BUFFER, 12 * sizeof (float), points, GL_STATIC_DRAW);
+  
+  GLuint vao = 0;
+  glGenVertexArrays (1, &vao);
+  glBindVertexArray (vao);
+  glEnableVertexAttribArray (0);
+  glBindBuffer (GL_ARRAY_BUFFER, vbo);
+  glVertexAttribPointer (0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+  std::string vertex_shader_str = readFile("../lab4-1_vs.glsl");
+  std::string fragment_shader_str = readFile("../lab4-1_fs.glsl");
+  const char *vertex_shader_src = vertex_shader_str.c_str();
+  const char *fragment_shader_src = fragment_shader_str.c_str();
+
+  GLuint vs = glCreateShader (GL_VERTEX_SHADER);
+  glShaderSource (vs, 1, &vertex_shader_src, NULL);
+  glCompileShader (vs);
+  checkShaderCompileError(vs);
+
+  GLuint fs = glCreateShader (GL_FRAGMENT_SHADER);
+  glShaderSource (fs, 1, &fragment_shader_src, NULL);
+  glCompileShader (fs);
+  checkShaderCompileError(fs);
+
+  GLuint shader_program = glCreateProgram ();
+  glAttachShader (shader_program, fs);
+  glAttachShader (shader_program, vs);
+  glLinkProgram (shader_program);
+  glDeleteShader(vs);
+  glDeleteShader(fs);
+
+  float start_time = glfwGetTime();
+
+  glUseProgram (shader_program);
+  glBindVertexArray (vao);
+  update_camera_basis();
+
+  while (!glfwWindowShouldClose (window)) 
+  {
+
+    GLsync s;
+
+    glfwGetFramebufferSize(window, &w_width , &w_height );
+        
+    // update other events like input handling 
+    glfwPollEvents ();
+
+    glUniform1f(glGetUniformLocation(shader_program, "i_global_time"),  glfwGetTime()-start_time);
+    glUniform4f(glGetUniformLocation(shader_program, "i_mouse_state"),  mouse_state.x,  mouse_state.y, mouse_state.z, mouse_state.w);
+    glUniform2f(glGetUniformLocation(shader_program, "i_window_size"),  w_width, w_height);
+    
+    glUniform3f(glGetUniformLocation(shader_program, "i_position"),       position.x, position.y, position.z);
+    glUniform3f(glGetUniformLocation(shader_program, "i_up"),             up.x,  up.y, up.z);
+    glUniform3f(glGetUniformLocation(shader_program, "i_dir"),            dir.x,  dir.y, dir.z);
+    glUniform3f(glGetUniformLocation(shader_program, "i_right"),          right.x,  right.y, right.z);
+    glUniform1f(glGetUniformLocation(shader_program, "i_focal_dist"),     focal_dist);
+    glUniform3f(glGetUniformLocation(shader_program, "i_light_position"), 6e3,  1e4, 1e4);
+    
+    // clear the drawing surface
+    glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    double tic = glfwGetTime();
+    
+    glDrawArrays(GL_TRIANGLE_STRIP,0, 4);
+    
+    double tic_toc = glfwGetTime() - tic;
+    if(tic_toc > 1.0) break;
+
+    glfwSwapBuffers (window);
+  }
+
+  // close GL context and any other GLFW resources
+  glfwTerminate();
+  exit(EXIT_SUCCESS);
+}
---
+++ LAB4/Lab4/lab4-1_fs.glsl
@@ -0,0 +1,407 @@
+#version 400
+out vec4 o_fragment_color;
+
+uniform vec2 i_window_size;
+uniform float i_global_time;
+uniform vec4 i_mouse_state;
+
+uniform vec3 i_position;
+uniform vec3 i_up;
+uniform vec3 i_right;
+uniform vec3 i_dir;
+uniform vec3 i_light_position;
+uniform float i_focal_dist;
+
+#define NUM_SPHERES 5
+#define MAX_DEPTH 10
+
+struct Ray { vec3 origin, dir; float weight;};
+
+//-------------------------------------------------------------------------//
+// Keep a global stack of rays for recursion
+
+Ray ray_stack[MAX_DEPTH];
+int ray_stack_size = 0;
+
+void push( Ray ray )
+{
+  if (ray_stack_size < MAX_DEPTH)
+  {
+    ray_stack[ray_stack_size] = ray;
+    ray_stack_size++;
+  }
+  //else stack overflow -- silently ignore
+}
+
+Ray pop()
+{
+  if (ray_stack_size > 0)
+  {
+    ray_stack_size--;
+    return ray_stack[ray_stack_size];
+  }
+  // else stack underflow -- silently ignore
+}
+
+//-------------------------------------------------------------------------//
+
+struct Material{
+  vec3 color_emission;
+  vec3 color_diffuse;
+  vec3 color_glossy;
+  float roughness;
+  float reflection;
+  float transmission;
+  float ior;  
+};
+
+struct Sphere {
+  float radius;
+  vec3 center;
+  Material material;
+};
+
+struct Plane {
+  float offset;
+  vec3 normal;
+  Material material;
+};
+
+struct Intersection
+{
+  vec3 point;
+  vec3 normal;
+  Material material;
+};
+
+struct Scene {
+
+  Sphere spheres[NUM_SPHERES];
+  Plane ground_plane[1];
+  vec3 sun_position;
+  float sun_brightness;
+
+};
+
+
+
+#define M_PI 3.14159
+
+// Insert your fancy BRDF from Lab 2 here!
+
+vec3 lambertian_brdf( vec3 in_direction, vec3 out_direction, Intersection isec )
+{
+  return vec3(isec.material.color_diffuse/M_PI);
+}
+
+
+Scene scene;
+
+void init( vec3 sun_pos, float sun_bright)
+{
+  // Initialise 5 spheres and a ground plane
+  
+  scene.spheres[0].center = vec3(0, 0.3, 0.5) ; 
+  scene.spheres[0].radius = 0.3;
+  scene.spheres[0].material.color_diffuse = vec3( 0.3, 0.1, 0.1 );
+  scene.spheres[0].material.color_glossy = 0.0 * vec3( 1 );
+  scene.spheres[0].material.color_emission = vec3( 0 );
+  scene.spheres[0].material.reflection = 0.1;
+  scene.spheres[0].material.transmission = 0;
+  scene.spheres[0].material.ior = 1;
+  
+  scene.spheres[1].center = vec3(0.8, 0.3, 0.8) ;
+  scene.spheres[1].radius = 0.3;
+  scene.spheres[1].material.color_diffuse = 0.5 * vec3( 0.0, 1.0, 0.0 );
+  scene.spheres[1].material.color_glossy = 0.5 * vec3( 1 );
+  scene.spheres[1].material.color_emission = vec3( 0 );
+  scene.spheres[1].material.reflection = 0.25;
+  scene.spheres[1].material.transmission = 0.25;
+  scene.spheres[1].material.ior = 1.52;
+
+  scene.spheres[2].center = vec3(0.55, 0.1, 0.2) ;
+  scene.spheres[2].radius = 0.1;
+  scene.spheres[2].material.color_diffuse = 1.0 * vec3( 0.0, 0.0, 0.0 );
+  scene.spheres[2].material.color_glossy = 0.0 * vec3( 1 );
+  scene.spheres[2].material.color_emission = vec3( 3.6, 0, 0 );
+  scene.spheres[2].material.reflection = 0.0;
+  scene.spheres[2].material.transmission = 0;
+  scene.spheres[2].material.ior = 1;
+
+  scene.spheres[3].center = vec3(0.7, 0.8, -0.5) ;
+  scene.spheres[3].radius = 0.8;
+  scene.spheres[3].material.color_diffuse = 0.5 * vec3( 0.2, 0.2, 0.15 );
+  scene.spheres[3].material.color_glossy = 0.5 * vec3( 1 );
+  scene.spheres[3].material.roughness = 10;
+  scene.spheres[3].material.color_emission = vec3( 0 );
+  scene.spheres[3].material.reflection = 0.0;
+  scene.spheres[3].material.transmission = 0;
+  scene.spheres[3].material.ior = 1;
+
+  scene.spheres[4].center = vec3(-0.65, 0.6, -1) ;
+  scene.spheres[4].radius = 0.6;
+  scene.spheres[4].material.color_diffuse = 0.5 * vec3( 0.5, 0.4, 0.25 );
+  scene.spheres[4].material.color_glossy = 0.5 * vec3( 1 );
+  scene.spheres[4].material.roughness = 1000;
+  scene.spheres[4].material.color_emission = vec3( 0 );
+  scene.spheres[4].material.reflection = 0.0;
+  scene.spheres[4].material.transmission = 0;
+  scene.spheres[4].material.ior = 1;
+
+  scene.ground_plane[0].normal = vec3(0,1,0);
+  scene.ground_plane[0].offset = 0;
+  scene.ground_plane[0].material.color_diffuse = 1.0 * vec3( 0.6 );
+  scene.ground_plane[0].material.color_glossy = 0.0 * vec3( 1 );
+  scene.ground_plane[0].material.color_emission = vec3( 0 );
+  scene.ground_plane[0].material.reflection = 0.0;
+  scene.ground_plane[0].material.transmission = 0;
+  scene.ground_plane[0].material.ior = 1;
+  
+
+  // Hard-coded single point light source
+  scene.sun_position = sun_pos;
+  scene.sun_brightness = max(sun_bright,0);
+}
+
+vec3 simple_sky(vec3 direction)
+{
+  // Also include a textured sky sphere, for niceness
+  float emission_sky = 1e-1*scene.sun_brightness;
+  float emission_sun = 1.0*scene.sun_brightness*scene.sun_brightness;
+  vec3 sky_color = vec3(0.35, 0.65, 0.85);
+  vec3 haze_color = vec3(0.8, 0.85, 0.9);
+  vec3 light_color = vec3(1);
+
+  float sun_spread = 2500.0;
+  float haze_spread = 1.6;
+  float elevation = acos(direction.y);
+    
+  float angle = abs(dot(direction, normalize(scene.sun_position)));
+  float response_sun = pow(angle, sun_spread);
+  float response_haze = pow(elevation, haze_spread);
+
+  vec3 sun_component = mix(emission_sky*sky_color, emission_sun*light_color,response_sun);
+  vec3 haze_component = mix(vec3(0),  emission_sky*haze_color,response_haze);
+
+  return (sun_component+haze_component);
+}
+
+
+// Ray-sphere intersection
+float intersect(Ray ray, Sphere s) 
+{
+  vec3 op = s.center - ray.origin;
+  float t;
+  float epsilon = 1e-6;
+  float b = dot(op, ray.dir);
+  float det = b * b - dot(op, op) + s.radius*s.radius;
+    
+  if (det < 0.) return 0.; 
+  else det = sqrt(det);
+  return (t = b - det) > epsilon ? t : ((t = b + det) > epsilon ? t : 0.);
+}
+
+// Ray-plane intersection
+float intersect(Ray ray, Plane p) 
+{
+  float a = dot(ray.origin, p.normal);
+  float b = dot(ray.dir, p.normal);
+  return (-a + p.offset)/b;
+}
+
+// Check for intersection of a ray and all objects in the scene
+Intersection intersect( Ray ray)
+{
+  Intersection I;
+  float t = 1e32;
+  int id = -1;
+    
+  //HIT SPHERES
+  for (int i = 0; i < NUM_SPHERES; ++i) {
+    float d = intersect(ray,scene.spheres[i]);
+    if (d!=0. && d<t) {
+      t = d; 
+      id = i;
+    }
+  }
+  I.point = t * ray.dir + ray.origin;
+  I.normal = normalize(I.point-scene.spheres[id].center);
+  I.material = scene.spheres[id].material;
+
+  //HIT PLANE
+  {
+    float d = intersect(ray,scene.ground_plane[0]);
+    if (d>0 && d<=t){
+      t=d;
+      I.point = t * ray.dir + ray.origin;
+      I.normal = scene.ground_plane[0].normal;
+      I.material = scene.ground_plane[0].material;
+      // Make checkered plane:
+      I.material.color_diffuse = (mod(floor(I.point.x) + floor(I.point.z),2.0) == 0.0) ?
+        scene.ground_plane[0].material.color_diffuse :
+        vec3(1.0) - scene.ground_plane[0].material.color_diffuse;
+    }
+  }
+
+  //HIT SKY
+  if (t>1e20){
+    I.point = ray.dir*t;
+    I.normal = -ray.dir;
+    vec3 sky = simple_sky(ray.dir);
+    I.material.color_diffuse = sky;
+    I.material.color_emission = 0.2*sky;
+    I.material.color_glossy = vec3(0);
+    I.material.reflection = 0;
+    I.material.transmission = 0;
+  }
+
+  return I;
+}
+
+vec3 raytrace() 
+{
+  vec3 ambient = 0.2*vec3(0.3, 0.6, 0.75);   // global ambient light
+  vec3 color = vec3(0);
+
+
+  {
+    // Iterate over all rays on the stack
+    for(int ray_stack_pos=0; ray_stack_pos < ray_stack_size; ++ray_stack_pos)
+    {
+
+      Ray ray = ray_stack[ray_stack_pos];
+      Intersection isec = intersect(ray);
+
+      vec3 nl = isec.normal * sign(-dot(isec.normal, ray.dir)); 
+      vec3 light_direction = normalize(scene.sun_position - isec.point);
+
+      vec3 this_color = vec3(0);
+            
+      float reflectivity = isec.material.reflection;
+
+      if (isec.material.transmission > 0)
+      {
+        // YOUR TASK: Create new ray, compute its position, its
+        // direction (based on isec.material.ior) and its weight, and
+        // call push(new_ray).
+
+        // Optionally, compute what fraction should be reflected, and
+        // send out a second ray in the reflection
+        // direction. Otherwise, use the block below for specular
+        // reflection.
+        
+      }
+      
+      if (isec.material.reflection > 0)
+      {
+        // YOUR TASK: Create new ray, compute its position, direction,
+        // and weight, and call push(ray).
+        vec3 reflect_dir = reflect(normalize(ray.dir), isec.normal);
+        Ray reflect_ray = Ray(isec.point+reflect_dir*1e-4, reflect_dir, isec.material.reflection); 
+        push(reflect_ray);
+      }
+
+      
+      // Now handle non-specular scattering (i.e., the non-recursive case)
+      {
+
+        // YOUR TASK: Create a "shadow feeler" ray and check if this
+        // point is visible from the (single) light source.
+        
+        vec3 light_dir = scene.sun_position-isec.point;
+        float light_distance = length(light_dir);
+        light_dir /= light_distance; //just a normalization, since we had the norm already.
+
+        Ray light_ray = Ray(isec.point+light_dir*1e-4, light_dir , 1.0); 
+        Intersection to_light_isec = intersect(light_ray);
+
+        vec3 irradiance = vec3(0.1 * scene.sun_brightness);
+    
+        if(length(to_light_isec.point - isec.point) <= light_distance)
+        {
+          irradiance = ambient;       
+        }
+
+          float cosine = dot(nl, light_direction);
+          this_color += irradiance * lambertian_brdf( light_direction, -ray.dir, isec ) * cosine;
+          this_color += isec.material.color_emission;
+          this_color *= ray.weight;
+          color += this_color;
+            
+        
+        
+      }
+
+
+    }
+  }
+
+  return color;
+}
+
+void main() {
+
+  // The code below creates a ray using the pixel coordinates for
+  // the current fragment: gl_FragCoord.
+  
+  //from coordinates in [0,0] to [w,h] range
+  //to coordinates in [-w/2,-h/2] to [w/2, h/2] range
+  vec2 uv =  gl_FragCoord.xy - 0.5*i_window_size.xy;
+  
+  init(i_light_position,
+       i_mouse_state.y // Enable to change sun brightness with scroll wheel
+       //10.0 // fixed sun brightness
+       ); 
+
+  //basis for defining the image plane
+  vec3 cx = i_right;
+  vec3 cy = i_up;   
+  vec3 cz = i_dir;  
+
+  //crude zooming by pressing right mouse button
+  float f_dist = i_focal_dist + i_focal_dist*i_mouse_state.w; 
+    
+  int num_samples=0;
+
+  Ray ray;
+  ray.origin = i_position;
+  ray.dir = normalize( uv.x*cx + uv.y*cy + cz*f_dist);
+  ray.weight = 1;
+  push( ray );
+    
+  vec3 color = raytrace(); num_samples++;
+    
+  //Antialiasing: shoot several rays per pixel.
+  ray.origin = i_position;
+  ray.dir = normalize( (uv.x+0.5)*cx + uv.y*cy + cz*f_dist);
+  ray.weight = 1;
+  ray_stack_size = 0; // clear stack
+  push( ray );
+  color += raytrace(); num_samples++;
+  ray.origin = i_position;
+  ray.dir = normalize( (uv.x-0.5)*cx + uv.y*cy + cz*f_dist);
+  ray.weight = 1;
+  ray_stack_size = 0; // clear stack
+  push( ray );
+  color += raytrace(); num_samples++;
+  ray.origin = i_position;
+  ray.dir = normalize( (uv.x)*cx + (uv.y+0.5)*cy + cz*f_dist);
+  ray.weight = 1;
+  ray_stack_size = 0; // clear stack
+  push( ray );
+  color += raytrace(); num_samples++;
+  ray.origin = i_position;
+  ray.dir = normalize( (uv.x)*cx + (uv.y-0.5)*cy + cz*f_dist);
+  ray.weight = 1;
+  ray_stack_size = 0; // clear stack
+  push( ray );
+  color += raytrace(); num_samples++;
+
+  // Finally, blend the result of the rays.
+
+  //linear blend
+  //o_fragment_color =  vec4((color),1);
+
+  //gamma corrected
+  o_fragment_color = vec4( pow ( clamp(color.xyz/num_samples, 0., 1.), vec3(1./2.2)), 1.);
+}
